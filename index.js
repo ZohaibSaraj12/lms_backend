934580e627462fa492d6198b254ae567b8c5258c
Muhammad Zohaib <zohaibsaraj10@gmail.com>
2025-12-22 16:39:43 +0500

    Railway Config

--- index.js
+++ index.js
@@ -7,13 +7,11 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
+  host: 'localhost',
+  user: 'root',
+  password: '12345',
+  database: 'mini_lms'
 });
-
 
 db.connect(err => {
   if (err) throw err;
